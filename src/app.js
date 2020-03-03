9aab17fbb075d5a306e2cb3d792ebda8066d2e44
Badma <andraev@ya.ru>
2020-03-03 17:48:24 +0300

    npm eslint -- --fix

--- src/app.js
+++ src/app.js
@@ -1,20 +1,10 @@
-"use strict";
+const characters = [
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'маг', health: 0 },
+  { name: 'лучник', health: 0 },
+];
 
-require("core-js/modules/es.array.filter");
+const alive = characters.filter((item) => item.health > 0);
 
-var characters = [{
-  name: 'мечник',
-  health: 10
-}, {
-  name: 'маг',
-  health: 100
-}, {
-  name: 'маг',
-  health: 0
-}, {
-  name: 'лучник',
-  health: 0
-}];
-var alive = characters.filter(function (item) {
-  return item.health > 0;
-});
+alive('маг');
